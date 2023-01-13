39cb22a9bde2c61df4b059848bf93ef1ad021220
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-13 15:50:47 -0800

    Remove ? from geology id tag and now can see console readouts.

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,16 @@ console.log("check")
 function handleSelect(event) {
   event.preventDefault();
   //may need to change this to const w/ specific name, with input
-  const coffee = parseInt(document.getElementById("coffee").value);
+  const selection1 = parseInt(document.getElementById("coffee").value);
   const selection2 = parseInt(document.getElementById("reptiles").value);
   const selection3 = parseInt(document.getElementById("geology").value);
-  console.log(coffee)
+  console.log(selection1)
+  console.log("test")
 
-  if (coffee && selection2 && selection3) {
-    if (coffee === selection2 && selection2 == selection3) {
+  if (selection1 && selection2 && selection3) {
+    if (selection1 === selection2 && selection2 == selection3) {
       document.getElementById("JavaScript").removeAttribute("class");
-    } else if (coffee === selection2 && selection2 !== selection3 || selection2 === selection3 && coffee !== selection3 || coffee === selection3 && coffee !== selection2) {
+    } else if (selection1 === selection2 && selection2 !== selection3 || selection2 === selection3 && selection1 !== selection3 || selection1 === selection3 && selection1 !== selection2) {
       document.getElementById("Python").removeAttribute("class");
     } else if (coffee !== selection2 && coffee !== selection3) {
       document.getElementById("Ruby").removeAttribute("class");
