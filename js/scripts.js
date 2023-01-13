3914854ebad7e837cf407b7c6e43b3e35416d37a
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-13 14:42:21 -0800

    Update const declarations -- order with parseInt and document.getElementById was incorrect; update selection1 to coffee; devTools console still gives this variable as undefined, but now reads it as number.

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,17 @@ function hideResultsAndError() {
 function handleSelect(event) {
   event.preventDefault();
   //may need to change this to const w/ specific name, with input
-  const selection1 = document.getElementById(parseInt("input[name='coffee']:checked").value);
-  const selection2 = document.getElementById(parseInt("input#reptiles").value);
-  const selection3 = document.getElementById(parseInt("geology").value);
+  const coffee = parseInt(document.getElementById("coffee").value);
+  const selection2 = parseInt(document.getElementById("reptiles").value);
+  const selection3 = paresInt(document.getElementById("geology").value);
 }
 
-  if (selection1 && selection2 && selection3) {
-    if (selection1 === selection2 && selection2 == selection3) {
+  if (coffee && selection2 && selection3) {
+    if (coffee === selection2 && selection2 == selection3) {
       document.getElementById("JavaScript").removeAttribute("class");
-    } else if (selection1 === selection2 && selection2 !== selection3 || selection2 === selection3 && selection1 !== selection3 || selection1 === selection3 && selection1 !== selection2) {
+    } else if (coffee === selection2 && selection2 !== selection3 || selection2 === selection3 && coffee !== selection3 || coffee === selection3 && coffee !== selection2) {
       document.getElementById("Python").removeAttribute("class");
-    } else if (selection1 !== selection2 && selection1 !== selection3) {
+    } else if (coffee !== selection2 && coffee !== selection3) {
       document.getElementById("Ruby").removeAttribute("class");
     } 
     } else {
