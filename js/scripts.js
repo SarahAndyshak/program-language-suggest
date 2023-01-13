41d69074ff303f76ae356a9519d8dd759aa629ce
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-13 09:13:41 -0800

    Add button to index.html, add hideResultsAndError to scripts.js, realized logic of form is going to need a lot of work.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//Business logic, I think
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
+  document.getElementById("JavaScript").setAttribute("class", "hidden");
+  document.getElementById("Python").setAttribute("class", "hidden");
+  document.getElementById("Ruby").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+  hideResultsAndError();
+}
+
+//User Interface logic, I think
