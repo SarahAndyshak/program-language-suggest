8fd87750f0a7bea41c1c2a879e61ae65dde1113a
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-13 16:21:13 -0800

    Add reset buttn functions to scripts.js, add const selection4 and selection5 in order to clear form with reset.

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,8 @@ function handleSelect(event) {
   const selection1 = parseInt(document.getElementById("coffee").value);
   const selection2 = parseInt(document.getElementById("reptiles").value);
   const selection3 = parseInt(document.getElementById("geology").value);
+  const selection4 = document.getElementById("forms");//added in for reset button function
+  const selection5 = document.getElementById("computers");
 
   if (selection1 && selection2 && selection3) {
     if (selection1 === selection2 && selection2 == selection3) {
@@ -29,5 +31,16 @@ function handleSelect(event) {
 
 window.addEventListener("load", function() {
   const form = document.getElementById("answers");
+  let resetBtn = document.querySelector("button#reset"); //reset button starts here
   form.addEventListener("submit", handleSelect);
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  });
+  resetBtn.addEventListener("click", function() {
+    document.getElementById("coffee").value = null;
+    document.getElementById("reptiles").value = null;
+    document.getElementById("geology").value = null;
+    document.getElementById("forms").value = null;
+    document.getElementById("computers").value = null;
+  });
 });
