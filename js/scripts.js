0a94745ec5a6536668b2693fea69ed4d94f395ae
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-21 17:39:55 -0800

    Correct name of element from coffee to selection1. Combine code resetting values to null into hideResultsAndError. Deleted unncessary references to survey questions 4 and 5. All in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 //Business logic, I think
 function hideResultsAndError() {
-  //document.getElementById("error-message").setAttribute("class", "hidden");
   document.getElementById("JavaScript").setAttribute("class", "hidden");
   document.getElementById("Python").setAttribute("class", "hidden");
   document.getElementById("Ruby").setAttribute("class", "hidden");
+  document.getElementById("coffee").value = null; // moved up
+  document.getElementById("reptiles").value = null;
+  document.getElementById("geology").value = null;
+  document.getElementById("forms").value = null;
+  document.getElementById("computers").value = null;
 }
 
 //updated code using sample from lesson on forms
@@ -12,15 +16,15 @@ function handleSelect(event) {
   const selection1 = parseInt(document.getElementById("coffee").value);
   const selection2 = parseInt(document.getElementById("reptiles").value);
   const selection3 = parseInt(document.getElementById("geology").value);
-  const selection4 = document.getElementById("forms");//added in for reset button function
-  const selection5 = document.getElementById("computers");
+  //const selection4 = document.getElementById("forms");//added in for reset button function
+  //const selection5 = document.getElementById("computers");
 
   if (selection1 && selection2 && selection3) {
     if (selection1 === selection2 && selection2 == selection3) {
       document.getElementById("JavaScript").removeAttribute("class");
     } else if (selection1 === selection2 && selection2 !== selection3 || selection2 === selection3 && selection1 !== selection3 || selection1 === selection3 && selection1 !== selection2) {
       document.getElementById("Python").removeAttribute("class");
-    } else if (coffee !== selection2 && coffee !== selection3) {
+    } else if (selection1 !== selection2 && selection1 !== selection3) {
       document.getElementById("Ruby").removeAttribute("class");
     } 
     } else {
@@ -33,20 +37,9 @@ window.addEventListener("load", function() {
   let resetBtn = document.querySelector("button#reset"); //reset button starts here
   form.addEventListener("submit", handleSelect);
   form.addEventListener("submit", function() {
-    //hideResultsAndError();
-    //handleSelect();
-    resetBtn.removeAttribute("class");
-  
+    resetBtn.removeAttribute("class");  
   });
   resetBtn.addEventListener("click", function() {
-    //add hideResultsAndError();
-    //document.getElementById("JavaScript").setAttribute("class", "hidden");//hides response, at this point, had to do it for each div id. Tried creating overarching div, but then the results didn't show up -- to refactor need to add in extra command about removing class on submit, needs more research and time.
-    //document.getElementById("Python").setAttribute("class", "hidden");
-    //document.getElementById("Ruby").setAttribute("class", "hidden");
-    document.getElementById("coffee").value = null;
-    document.getElementById("reptiles").value = null;
-    document.getElementById("geology").value = null;
-    document.getElementById("forms").value = null;
-    document.getElementById("computers").value = null;
+    hideResultsAndError();
   });
 });
