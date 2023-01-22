c369390a012c20fcec32dde30e0261b78e4a2be2
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-22 12:55:12 -0800

    Rearrange handleSelect to include hideResultsAndError in order to re-hide and display suggested language.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 //Business logic, I think
+
 function hideResultsAndError() {
   document.getElementById("JavaScript").setAttribute("class", "hidden");
   document.getElementById("Python").setAttribute("class", "hidden");
   document.getElementById("Ruby").setAttribute("class", "hidden");
-  document.getElementById("coffee").value = null; // moved up
-  document.getElementById("reptiles").value = null;
-  document.getElementById("geology").value = null;
-  document.getElementById("forms").value = null;
-  document.getElementById("computers").value = null;
 }
+// function hideDropDownValues() {
+//   document.getElementById("coffee").value = null; // moved up - not necessary
+//   document.getElementById("reptiles").value = null;
+//   document.getElementById("geology").value = null;
+//   document.getElementById("forms").value = null;
+//   document.getElementById("computers").value = null;
+// }
 
 //updated code using sample from lesson on forms
 function handleSelect(event) {
   event.preventDefault();
+  hideResultsAndError();
   const selection1 = parseInt(document.getElementById("coffee").value);
   const selection2 = parseInt(document.getElementById("reptiles").value);
   const selection3 = parseInt(document.getElementById("geology").value);
@@ -32,13 +36,13 @@ function handleSelect(event) {
 
 window.addEventListener("load", function() {
   const form = document.getElementById("answers");
-  hideResultsAndError(); // added to test- this sets the form as blank and resets the form, but if used still shows all results
-  let resetBtn = document.querySelector("button#reset"); //reset button starts here
+//   // hideResultsAndError(); // added to test- this sets the form as blank and resets the form, but if used still shows all results
+//   // let resetBtn = document.querySelector("button#reset"); //reset button starts here, now omitted
   form.addEventListener("submit", handleSelect);
-  form.addEventListener("submit", function() {
-    resetBtn.removeAttribute("class");  
-  });
-  resetBtn.addEventListener("click", function() {
-    hideResultsAndError();
-  });
+//   // form.addEventListener("submit", function() {
+//   //   resetBtn.removeAttribute("class");  
+//   // });
+//   // resetBtn.addEventListener("click", function() {
+//   //   hideResultsAndError();
+//   // });
 });
